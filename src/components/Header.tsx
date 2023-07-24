b8891f77b9f1eaa7116a7d5adbb8ced33f7fc15d
walde-dev <paninwaldemar@gmail.com>
2023-07-24 15:43:22 +0200

    add home redirect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="fixed top-0 z-50 flex w-full md:px-60 md:pt-10">
-      <div className="flex flex-row justify-between w-full items-center">
+      <div className="flex w-full flex-row items-center justify-between">
         <Logo />
         <div className="flex space-x-1">
           <HeaderLink href="kalender"> kalender </HeaderLink>
@@ -14,10 +16,10 @@ export default function Header() {
 
 export function Logo() {
   return (
-    <span className="text-2xl">
+    <Link href="/" className="text-2xl">
       punchline<span className="font-bold text-primary">/</span>
       <span className="font-bold">quiz</span>
-    </span>
+    </Link>
   );
 }
 
@@ -29,7 +31,10 @@ export function HeaderLink({
   children: React.ReactNode;
 }) {
   return (
-    <a href={href} className="text-xl rounded-md py-2 px-4 hover:bg-gray-800 transition-all duration-300 ease-in-out">
+    <a
+      href={href}
+      className="rounded-md px-4 py-2 text-xl transition-all duration-300 ease-in-out hover:bg-gray-800"
+    >
       <span className="text-primary">/</span>
       {children}
     </a>
